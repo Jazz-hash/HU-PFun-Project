6d8e892395bf3b415a7b0e2ab0e4fbf410d05cfa
Jazz-hash <jazzelmehmood6@gmail.com>
2020-11-26 18:58:04 +0500

    DONE git add .git add .

--- app/server.py
+++ app/server.py
@@ -1,10 +1,15 @@
+
+# ? imports ( Flask, Selectorlib)
 from flask import Flask, render_template, request
 from selectorlib import Extractor
 import requests 
 import json 
 
+# ? Creating app with Flask
 app = Flask(__name__)
 
+# ? YML string to grab data from website
+# ? Specifying css classes, ids and attributes of text to get from the site.
 yml_string = """
 products:
     css: 'div[data-component-type="s-search-result"]'
@@ -36,9 +41,12 @@ products:
             type: Text
 """
 
+# ? Created extractor from YML string with the help of Selectorlib.
 e = Extractor.from_yaml_string(yml_string)
 
+# ? Scraping function to make the thing work modular.
 def scrape(url):  
+    # ? Defining headers to bypass "Are you Robot" verifications.
     headers = {
         'dnt': '1',
         'upgrade-insecure-requests': '1',
@@ -52,31 +60,42 @@ def scrape(url):
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
 
+    # ? Sending request to the url.
     r = requests.get(url, headers=headers)
+    # ? Verifying sever response Error with codes.
     if r.status_code > 500:
         return None
+    # ? Extrating data from the repsonse obtained from the request.
     return e.extract(r.text)
 
-
+# ? Index page route.
 @app.route('/')
 def index():
+    # ? rendering htm file.
     return render_template('index.htm')
 
+# ? Search page route.
 @app.route('/search')
 def search_results():
+    # ? getting s (search_query) variable from the url.
     search = request.args.get('s')
+    # ? defined url
     url_amazon = "https://www.amazon.com/s?k=" + search
     full_dict = []
+    # ? storing data returned from the function.
     data_amazon = scrape(url_amazon) 
+    # ? pushing data to dict for better error handling.
     if data_amazon:
         for product in data_amazon['products'][:12]:
             product['search_url'] = url_amazon
             full_dict.append(product)
     prices = sorted([float(item["price"][1:].replace(",", "")) for item in full_dict if item["price"]])
-
+    
+    # ? Getting max, mid and min prices from the data.
     max_price = "${:,.2f}".format(max(prices))
     mid_price = "${:,.2f}".format(prices[len(prices) // 2])
     min_price = "${:,.2f}".format(min(prices))
     print(min_price, max_price, mid_price)
 
+    # ? finally rendering everything to the htm file.
     return render_template('search.htm', search=search, products=full_dict, min_price=min_price, max_price=max_price, mid_price=mid_price)
